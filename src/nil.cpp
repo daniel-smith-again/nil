e4b6c86de9076291dd727775d162cd748dd19fc3
Daniel Smith <daniel.smith.again@gmail.com>
2025-10-22 22:38:45 -0400

    ...

---
+++ src/nil.cpp
@@ -0,0 +1,16 @@
+/*
+ *                  NIL Programming Language Implementation
+ *                          Copyright © Daniel Smith
+ */
+
+/*
+ * There's a few components.
+ */
+
+#include <fstream>
+#include <stdlib>
+
+void main (int argv, char** argc)
+{
+
+}
